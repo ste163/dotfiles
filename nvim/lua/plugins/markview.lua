8d508905afb9a9d8a569448370da1d182c49a4f5
Sam Edwards <samtedwards@pm.me>
2026-05-16 11:55:52 -0400

    updated nvim config

--- nvim/lua/plugins/markview.lua
+++ nvim/lua/plugins/markview.lua
@@ -3,3 +3,7 @@ vim.pack.add({ {
 	src = "https://github.com/OXY2DEV/markview.nvim",
 	version = "v28.2.0",
 } })
+
+require("markview").setup({
+	preview = { icon_provider = "devicons" },
+})
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,6 +13,15 @@ vim.pack.add({
 	},
 })
 
+require("telescope").setup({
+    pickers = {
+        find_files = {
+            hidden = true,
+            file_ignore_patterns = { ".git/" },
+        },
+    },
+})
+
 local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
